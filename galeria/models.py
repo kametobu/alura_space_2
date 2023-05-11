0c88cd942f72d1eb03595900f9500f2d1e7ba5a1
Kametobu <alannbatista1@gmail.com>
2023-05-11 12:06:30 -0300

    Alura Space 2

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/",blank=True)
+    opcoes_categoria = [
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALÁXIA","Galáxia"),
+        ("PLANETA","Planeta")
+    ]
+    categoria = models.CharField(max_length=100, choices=opcoes_categoria,default="")
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now(), blank=False)
+    def __str__(self):
+        return self.nome
